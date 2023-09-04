42538d3e71a5434d75f836763e9b2306ab6a71d7
PREMINDA <premindabandara0777@gmail.com>
2023-09-05 02:51:34 +0530

    add auth service

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -5,5 +5,39 @@ import { Injectable } from '@angular/core';
 })
 export class AuthService {
 
-  constructor() { }
+  constructor() {}
+
+  public setRoles(roles: []) {
+    localStorage.setItem('roles', JSON.stringify(roles));
+  }
+
+  public getRoles(): [] {
+    return JSON.parse(localStorage.getItem('roles') as string);
+  }
+
+  public setToken(jwtToken: string) {
+    localStorage.setItem('jwtToken', jwtToken);
+  }
+
+  public getToken(): string|null {
+    return localStorage.getItem('jwtToken');
+  }
+
+  public clear() {
+    localStorage.clear();
+  }
+
+  public isLoggedIn() {
+    return this.getRoles() && this.getToken();
+  }
+
+  public isAdmin() {
+    const roles: any[] = this.getRoles();
+    return roles[0].roleName === 'Admin';
+  }
+
+  public isUser() {
+    const roles: any[] = this.getRoles();
+    return roles[0].roleName === 'User';
+  }
 }
